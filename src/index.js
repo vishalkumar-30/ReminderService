80de957397aa1fd488fc76690339cadee6a500c2
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-06-22 19:30:01 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
 
-const {PORT} = require('./config/serverConfig');
+const {PORT} = require('./config/serverConfig'); 
+const {sendBasicEmail} = require('./services/email-service');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extrnded: true}));
+
     app.listen(PORT, () => {
         console.log(`server started at ${PORT}`);
+
+        sendBasicEmail(
+            'AirlineSupport@admin.com',
+            'Flightbookingappservice@gmail.com',
+            'This is a testing email',
+            'Hey, how are you, I ope you like the support'
+        );
+
+        cron.schedule('*/2 * * * *', () => {
+          console.log('running a task every two minutes');
+        });
     });
 }
 
